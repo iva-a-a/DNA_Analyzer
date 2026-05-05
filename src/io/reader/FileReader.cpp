a070a47c2d8ff71b0290c6667bac95334cdff768
satvajos <satvajos@student.21-school.ru>
2026-05-05 20:42:24 +0300

    добавлен метод чтения строк из файла

--- src/io/reader/FileReader.cpp
+++ src/io/reader/FileReader.cpp
@@ -2,12 +2,30 @@
 #include "../../core/common/errors/Errors.hpp"
 #include <fstream>
 
-std::string FileReader::readLine(const std::string &filePath) {
+template <typename Func>
+auto FileReader::withFile(const std::string &filePath, Func func) {
   std::ifstream file(filePath);
   if (!file.is_open()) {
     throw FileReadError("Could not open file: " + filePath);
   }
-  std::string line;
-  std::getline(file, line);
-  return line;
+  return func(file);
+}
+
+std::string FileReader::readLine(const std::string &filePath) {
+  return withFile(filePath, [](std::ifstream &file) {
+    std::string line;
+    std::getline(file, line);
+    return line;
+  });
+}
+
+std::vector<std::string> FileReader::readLines(const std::string &filePath) {
+  return withFile(filePath, [](std::ifstream &file) {
+    std::vector<std::string> lines;
+    std::string line;
+    while (std::getline(file, line)) {
+      lines.push_back(line);
+    }
+    return lines;
+  });
 }
