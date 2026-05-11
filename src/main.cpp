5b94dcd6c587f31e3b1e488688377387fa8ae5fe
satvajos <satvajos@student.21-school.ru>
2026-05-11 15:16:53 +0300

    добавлен main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "cli/dispatcher/CommandDispatcher.hpp"
+#include "cli/parser/ArgumentParser.hpp"
+
+int main(int argc, char *argv[]) {
+  auto options = ArgumentParser::parse(argc, argv);
+  CommandDispatcher dispatcher;
+  return dispatcher.dispatch(options);
+}
